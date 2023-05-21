60de45b457da60617f658bfc5ba15a8925f2f57e
Antonio Gennaro <antgennaro@hotmail.com>
2023-05-21 16:32:38 -0300

    1er intento de maquetado

---
+++ manejo.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+function eligeCantProd() {
+
+}
+
+function eligeCantPermit() {
+    
+}
+
+function eligeColores() {
+    
+}
+
+function generaCodigo(){
+    console.log("Presionó botón Genera Código");
+}
+
+function comprarProductox(){
+    console.log("Presionó botón comprar");
+}
